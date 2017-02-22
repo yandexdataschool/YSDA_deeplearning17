1051da960c4f0157fe6a6637bc29bcf255b252b0
jheuristic <justheuristic@gmail.com>
2017-02-22 19:55:13 +0300

    classwork

---
+++ Seminar3/classwork_auxiliary.py
@@ -0,0 +1,374 @@
+"""Some auxiliary files used for classwork assignment"""
+
+
+import warnings
+import numpy as np
+import numpy as np
+from random import randrange
+
+def eval_numerical_gradient(f, x, verbose=True, h=0.00001):
+  fx = f(x) # evaluate function value at original point
+  grad = np.zeros_like(x)
+  # iterate over all indexes in x
+  it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+  while not it.finished:
+
+    # evaluate function at x+h
+    ix = it.multi_index
+    oldval = x[ix]
+    x[ix] = oldval + h # increment by h
+    fxph = f(x) # evalute f(x + h)
+    x[ix] = oldval - h
+    fxmh = f(x) # evaluate f(x - h)
+    x[ix] = oldval # restore
+
+    # compute the partial derivative with centered formula
+    grad[ix] = (fxph - fxmh) / (2 * h) # the slope
+    if verbose:
+      print ix, grad[ix]
+    it.iternext() # step to next dimension
+
+  return grad
+
+
+def eval_numerical_gradient_array(f, x, df, h=1e-5):
+  grad = np.zeros_like(x)
+  it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+  while not it.finished:
+    ix = it.multi_index
+    
+    oldval = x[ix]
+    x[ix] = oldval + h
+    pos = f(x).copy()
+    x[ix] = oldval - h
+    neg = f(x).copy()
+    x[ix] = oldval
+    
+    grad[ix] = np.sum((pos - neg) * df) / (2 * h)
+    it.iternext()
+  return grad
+
+def rel_error(x, y):
+    return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
+
+warnings.filterwarnings("ignore")
+
+
+def sgd_momentum(x, dx, config, state):
+    """
+        This is a very ugly implementation of sgd with momentum 
+        just to show an example how to store old grad in state.
+        
+        config:
+            - momentum
+            - learning_rate
+        state:
+            - old_grad
+    """
+    
+    # x and dx have complex structure, old dx will be stored in a simpler one
+    state.setdefault('old_grad', {})
+    
+    i = 0 
+    for cur_layer_x, cur_layer_dx in zip(x,dx): 
+        for cur_x, cur_dx in zip(cur_layer_x,cur_layer_dx):
+            
+            cur_old_grad = state['old_grad'].setdefault(i, np.zeros_like(cur_dx))
+            
+            np.add(config['momentum'] * cur_old_grad, config['learning_rate'] * cur_dx, out = cur_old_grad)
+            
+            cur_x -= cur_old_grad
+            i += 1     
+
+
+class Module(object):
+    
+    def __init__ (self):
+        self.output = None
+        self.gradInput = None
+    
+    def forward(self, input):
+        """
+        Takes an input object, and computes the corresponding output of the module.
+        """
+        return self.updateOutput(input)
+
+    def backward(self,input, gradOutput):
+        """
+        Performs a backpropagation step through the module, with respect to the given input.
+        
+        This includes 
+         - computing a gradient w.r.t. `input` (is needed for further backprop),
+         - computing a gradient w.r.t. parameters (to update parameters while optimizing).
+        """
+        self.updateGradInput(input, gradOutput)
+        self.accGradParameters(input, gradOutput)
+        return self.gradInput
+    
+
+    def updateOutput(self, input):
+        """
+        Computes the output using the current parameter set of the class and input.
+        This function returns the result which is stored in the `output` field.
+        
+        Make sure to both store the data in `output` field and return it. 
+        """
+        
+        # The easiest case:
+            
+        # self.output = input 
+        # return self.output
+        
+        pass
+
+    def updateGradInput(self, input, gradOutput):
+        """
+        Computing the gradient of the module with respect to its own input. 
+        This is returned in `gradInput`. Also, the `gradInput` state variable is updated accordingly.
+        
+        The shape of `gradInput` is always the same as the shape of `input`.
+        
+        Make sure to both store the gradients in `gradInput` field and return it.
+        """
+        
+        # The easiest case:
+        
+        # self.gradInput = gradOutput 
+        # return self.gradInput
+        
+        pass   
+    
+    def accGradParameters(self, input, gradOutput):
+        """
+        Computing the gradient of the module with respect to its own parameters.
+        No need to override if module has no parameters (e.g. ReLU).
+        """
+        pass
+    
+    def zeroGradParameters(self): 
+        """
+        Zeroes `gradParams` variable if the module has params.
+        """
+        pass
+        
+    def getParameters(self):
+        """
+        Returns a list with its parameters. 
+        If the module does not have parameters return empty list. 
+        """
+        return []
+        
+    def getGradParameters(self):
+        """
+        Returns a list with gradients with respect to its parameters. 
+        If the module does not have parameters return empty list. 
+        """
+        return []
+    
+    def __repr__(self):
+        """
+        Pretty printing. Should be overrided in every module if you want 
+        to have readable description. 
+        """
+        return "Module"
+    
+class Criterion:
+    def forward(self, input, target):
+        """
+            Given an input and a target, compute the loss function 
+            associated to the criterion and return the result.
+            
+            For consistency this function should not be overrided,
+            all the code goes in `updateOutput`.
+        """
+        return self.updateOutput(input, target)
+
+    def backward(self, input, target):
+        """
+            Given an input and a target, compute the gradients of the loss function
+            associated to the criterion and return the result. 
+
+            For consistency this function should not be overrided,
+            all the code goes in `updateGradInput`.
+        """
+        return self.updateGradInput(input, target)
+    
+    def updateOutput(self, input, target):
+        """
+        Function to override.
+        """
+        return self.output
+
+    def updateGradInput(self, input, target):
+        """
+        Function to override.
+        """
+        return self.gradInput   
+
+    def __repr__(self):
+        """
+        Pretty printing. Should be overrided in every module if you want 
+        to have readable description. 
+        """
+        return "Criterion"
+    
+class Sequential(Module):
+    
+    """
+         This class implements a container, which processes `input` data sequentially. 
+         
+         `input` is processed by each module (layer) in self.modules consecutively.
+         The resulting array is called `output`. 
+    """
+    
+    def __init__ (self):
+        super(Sequential, self).__init__()
+        self.modules = []
+   
+    def add(self, module):
+        """
+        Adds a module to the container.
+        """
+        self.modules.append(module)
+
+    def updateOutput(self, input):
+        """
+        Basic workflow of FORWARD PASS:
+        
+            y_0    = module[0].forward(input)
+            y_1    = module[1].forward(y_0)
+            ...
+            output = module[n-1].forward(y_{n-2})   
+            
+            
+        Just write a little loop. 
+        """
+        
+        self.output = input
+        for module in self.modules:
+            module.y = self.output
+            self.output = module.forward(self.output)
+        return self.output
+
+    def backward(self, input, gradOutput):
+        """
+        Workflow of BACKWARD PASS:
+
+            g_{n-1} = module[n-1].backward(y_{n-2}, gradOutput)
+            g_{n-2} = module[n-2].backward(y_{n-3}, g_{n-1})
+            ...
+            g_1 = module[1].backward(y_0, g_2)   
+            gradInput = module[0].backward(input, g_1)   
+             
+             
+        !!!
+                
+        To ech module you need to provide the input, module saw while forward pass, 
+        it is used while computing gradients. 
+        Make sure that the input for `i-th` layer the output of `module[i]` (just the same input as in forward pass) 
+        and NOT `input` to this Sequential module. 
+        
+        !!!
+        
+        """
+        
+        self.curent_grad = gradOutput
+        
+        for module in reversed(self.modules):
+            self.curent_grad = module.backward(module.y, self.curent_grad)
+
+        return self.gradInput
+      
+
+    def zeroGradParameters(self): 
+        for module in self.modules:
+            module.zeroGradParameters()
+    
+    def getParameters(self):
+        """
+        Should gather all parameters in a list.
+        """
+        return [x.getParameters() for x in self.modules]
+    
+    def getGradParameters(self):
+        """
+        Should gather all gradients w.r.t parameters in a list.
+        """
+        return [x.getGradParameters() for x in self.modules]
+    
+    def __repr__(self):
+        string = "".join([str(x) + '\n' for x in self.modules])
+        return string
+    
+    def __getitem__(self,x):
+        return self.modules.__getitem__(x)
+    
+    
+    
+import sys
+import os
+import time
+
+import numpy as np
+
+__doc__="""taken from https://github.com/Lasagne/Lasagne/blob/master/examples/mnist.py"""
+
+def load_dataset():
+    # We first define a download function, supporting both Python 2 and 3.
+    if sys.version_info[0] == 2:
+        from urllib import urlretrieve
+    else:
+        from urllib.request import urlretrieve
+
+    def download(filename, source='http://yann.lecun.com/exdb/mnist/'):
+        print("Downloading %s" % filename)
+        urlretrieve(source + filename, filename)
+
+    # We then define functions for loading MNIST images and labels.
+    # For convenience, they also download the requested files if needed.
+    import gzip
+
+    def load_mnist_images(filename):
+        if not os.path.exists(filename):
+            download(filename)
+        # Read the inputs in Yann LeCun's binary format.
+        with gzip.open(filename, 'rb') as f:
+            data = np.frombuffer(f.read(), np.uint8, offset=16)
+        # The inputs are vectors now, we reshape them to monochrome 2D images,
+        # following the shape convention: (examples, channels, rows, columns)
+        data = data.reshape(-1, 1, 28, 28)
+        # The inputs come as bytes, we convert them to float32 in range [0,1].
+        # (Actually to range [0, 255/256], for compatibility to the version
+        # provided at http://deeplearning.net/data/mnist/mnist.pkl.gz.)
+        return data / np.float32(256)
+
+    def load_mnist_labels(filename):
+        if not os.path.exists(filename):
+            download(filename)
+        # Read the labels in Yann LeCun's binary format.
+        with gzip.open(filename, 'rb') as f:
+            data = np.frombuffer(f.read(), np.uint8, offset=8)
+        # The labels are vectors of integers now, that's exactly what we want.
+        return data
+
+    # We can now download and read the training and test set images and labels.
+    X_train = load_mnist_images('train-images-idx3-ubyte.gz')
+    y_train = load_mnist_labels('train-labels-idx1-ubyte.gz')
+    X_test = load_mnist_images('t10k-images-idx3-ubyte.gz')
+    y_test = load_mnist_labels('t10k-labels-idx1-ubyte.gz')
+
+    # We just return all the arrays in order, as expected in main().
+    # (It doesn't matter how we do this as long as we can read them again.)
+    
+    from sklearn.preprocessing import OneHotEncoder
+    encoder = OneHotEncoder(sparse=False).fit(y_train[:,None])
+
+    y_train = encoder.transform(y_train[:,None])
+    y_test = encoder.transform(y_test[:,None])
+    X_train = X_train.reshape([len(X_train),-1])
+    X_test = X_test.reshape([len(X_test),-1])
+    return X_train, y_train, X_test, y_test
+
+
+
+
